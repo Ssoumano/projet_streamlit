82a1b779ef6bf301cb619a3695a5a3fc3e6fd63c
Ssoumano <122726740+Ssoumano@users.noreply.github.com>
2024-05-07 11:06:56 +0200

    Update Page_accueil.py

--- Page_accueil.py
+++ Page_accueil.py
@@ -110,7 +110,7 @@ st.sidebar.markdown("<h1 style='text-align: right; text-transform: lowercase;'>Y
 
 
 # Chemin vers le fichier shapefile des régions
-shapefile_path = 'https://drive.google.com/file/d/1yU-46dXTPx1uuKDH-ro58NGbf6pjzlHV/view?usp=sharing'
+shapefile_path = 'regions-20180101.shp'
 
 # Charger le shapefile dans un GeoDataFrame
 gdf = gpd.read_file(shapefile_path)
